bc267dde7b1d0029a366d6e5845df3733dc65637
dos Santos, Diego <diego.dos.santos@sap.com>
2019-08-15 22:59:22 -0300

    Converted functions to JS and added authentication

---
+++ functions/index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express = require('express');
+const cookieParser = require('cookie-parser')();
+const cors = require('cors')({origin: true});
+const app = express();
+
+admin.initializeApp();
+
+const authenticate = async (req, res, next) => {
+    if (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) {
+        console.error("Request without the authorization headers.");
+        res.status(403).send('Unauthorized');
+        return;
+    }
+    
+    const idToken = req.headers.authorization.split('Bearer ')[1];
+    try {
+        console.debug("User logged successfully.");
+        const decodedIdToken = await admin.auth().verifyIdToken(idToken);
+        req.user = decodedIdToken;
+        next();
+        return;
+    } catch(e) {
+        console.error(`Error: ${e}`);
+        res.status(403).send('Unauthorized');
+        return;
+    }
+};
+
+app.use(cors);
+app.use(cookieParser);
+app.use(authenticate);
+app.get('/hello', (req, res) => {
+    res.send(`Hello ${req.user.name}`);
+});
+
+exports.api = functions.https.onRequest(app);
