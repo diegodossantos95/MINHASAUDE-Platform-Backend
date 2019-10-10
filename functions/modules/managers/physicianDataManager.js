aae57d0b2d30bb38369040ea907438eaf0c3cf57
dos Santos, Diego <diego.dos.santos@sap.com>
2019-10-10 00:09:49 -0300

    Fixed the getsharings in physiciandatamanager

--- functions/modules/managers/physicianDataManager.js
+++ functions/modules/managers/physicianDataManager.js
@@ -3,7 +3,7 @@
 const admin = require('firebase-admin');
 const db = admin.firestore();
 const physicianCollectionName = "physicians";
-const sharingCollectionName = "sharings";
+const sharingPropertyName = "sharings";
 
 const getSharings = sName => {
     //TODO: Handle if the document doesnt exist
@@ -11,14 +11,11 @@ const getSharings = sName => {
     return db
         .collection(physicianCollectionName)
         .doc(sName)
-        .collection(sharingCollectionName)
         .get()
-        .then(querySnapshot => {
-            const docs = querySnapshot.docs.map(document => {
-                return document.data();
-            });
+        .then(docSnapshot => {
+            const sharings = docSnapshot.get(sharingPropertyName);
 
-            return Promise.resolve(docs);
+            return Promise.resolve(sharings);
         })
         .catch(error => {
             return Promise.reject(error);
